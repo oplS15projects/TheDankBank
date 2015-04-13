1ca8a7451843730c11d37492f0a2b8cf968fff6b
Tyler Alterio <tyalt1@gmail.com>
2015-04-13 11:12:43 -0400

    Added use of Caesar cipher.

--- Test_Cipher_GUI.rkt
+++ Test_Cipher_GUI.rkt
@@ -17,7 +17,23 @@
 (define e-or-d-checkbox (new check-box% [label "Check to decrypt"]
                                         [parent panel-alpha]))
 
+(define (decrypt?) (if (send e-or-d-checkbox get-value) 'decrypt 'encrypt))
+
 #|Caesar Cipher|#
+(define panel-caesar (new horizontal-panel% [parent window]))
+
+(define text-caesar (new text-field% [label "Caesar Shift Value:"]
+                                     [parent panel-caesar]
+                                     [min-width 2]
+                                     [stretchable-width #f]))
+
+(define button-caesar (new button% [label "Use Caesar Cipher!"]
+                                   [parent panel-caesar]
+                                   [callback (lambda (b e) (send message
+                                                                 set-label
+                                                                 (caesar-cipher (send text get-value)
+                                                                                (string->number (send text-caesar get-value))
+                                                                                (decrypt?))))]))
 
 #|Vigenere Cipher|#
 
